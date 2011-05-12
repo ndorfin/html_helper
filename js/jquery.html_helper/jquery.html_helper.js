affb6c52880ebb344078d1ea6afefe237a023c43
Shaun O'Connell <ndorfin@gmail.com>
2011-05-13 00:20:01 +0200

    Added a rudimentary doctype detector so that I can start doing validation of the document based on its doctype - needs lots of refinement - its still early days.  Added a list of presentational elements and attributes worth throwing warnings over. Updated the test-cases.

--- js/jquery.html_helper/jquery.html_helper.js
+++ js/jquery.html_helper/jquery.html_helper.js
@@ -1,12 +1,42 @@
 /*! 
  * jquery.html_helper.js
  * -------------------------------
- * Description:	An HTML best-practices helper, for on-the-fly debugging of common HTML mistakes.
+ * Description:	An HTML best-practices helper, for on-the-fly highlighting of common HTML mistakes.
  * Author: 		Shaun O'Connell - shaun@tactile.co.za
  * More info: 	http://tactile.co.za
- * Requires: 	jQuery 1.3.2+
  */
 $(document).ready(function(){
+	log = function(obj){
+		if (window.console) {
+			window.console.log(obj);
+		}
+	};
+
+	// Debug info
+	if ($("html.debug") && !$.browser.msie){
+		var doc_type = "Unknown";
+		var doc_type_description = "<!DOCTYPE ";
+		if (window.document.doctype.name){
+			doc_type_description = doc_type_description + window.document.doctype.name;
+		}
+		if (window.document.doctype.publicId){
+			doc_type_description = doc_type_description + ' "' + window.document.doctype.publicId + '"';
+		}
+		if (window.document.doctype.systemId){
+			doc_type_description = doc_type_description + ' "' + window.document.doctype.systemId + '"';
+			if (window.document.doctype.systemId == "-//W3C//DTD HTML 4.01//EN"){
+				doc_type = "HTML4";
+			}
+		}
+		doc_type_description = doc_type_description + '>';
+		
+		if (doc_type_description == "<!DOCTYPE html>"){
+			doc_type = "HTML5";
+		}
+		log("Document type is: " + doc_type + ". (" + doc_type_description + ")");
+	}
+
+
 	var helper_debug_level = 'warning'; /* valid: error|warning */
 
 	var path_to_tooltips = "js/jquery.html_helper/css/tooltips.css"
@@ -22,6 +52,9 @@ $(document).ready(function(){
 			"selector": "a[href=\"#\"]",
 			"description": "This anchor (&lt;a&gt;) has an 'href' attribute with a '#' as a value. Ideally, you should use a valid ID-ref URI as the 'href' value."
 		},{
+			"selector": "a[target]",
+			"description": "This anchor (&lt;a&gt;) has a 'target' attribute. Rather avoid opening new windows or targetting open windows.  If you really have to, you should use JS to add such functionality to new window links."
+		},{
 			"selector": "img[alt=\"\"]",
 			"description": "This image (&lt;img&gt;) is missing an 'alt' attribute value.  Specify an 'alt' attribute so that non-sighted users can understand what this image is trying to convey.  If this image is purely decorational, then consider applying this image via CSS."
 		},{
@@ -33,6 +66,15 @@ $(document).ready(function(){
 		},{
 			"selector": "label[for=\"\"], label:not([for])",
 			"description": "You've used a label (&lt;label&gt;) here without specifying a 'for' attribute or attribute value.  Ideally, a label's 'for' attribute should be associated with a form field's ID."
+		},{
+			"selector": "b, big, center, hr, i, font, small, strike, u",
+			"description": "This element is considered a presentational element. Use the semantically preferred element and CSS to style rather than relying on the HTML to define the style here."
+		},{
+			"selector": "blink, marquee",
+			"description": "1999 called.  They want their &lt;blink&gt; and &lt;marquee&gt; elements back."
+		},{
+			"selector": "[align], *:not(img)[height], [valign], *:not(img)[width]",
+			"description": "This element contains a presentational attribute such as 'align' or 'width'.  These should be replaced with an appropriate CSS style rather."
 		}
 	]
 
@@ -56,6 +98,9 @@ $(document).ready(function(){
 		},{
 			"selector": "img:not([alt])",
 			"description": "This image (&lt;img&gt;) is missing an 'alt' attribute or attribute value.  Specify an 'alt' attribute so that non-sighted users can understand what this image is trying to convey."
+		},{
+			"selector": "img:not([src])",
+			"description": "This image (&lt;img&gt;) is missing a 'src' attribute.  Without a src attribute this image will not render appropriately."
 		},{
 			"selector": "form[action=\"\"], form:not([action])",
 			"description": "This form (&lt;form&gt;) is missing an 'action' attribute or attribute value. Specify a valid URL as the 'action' attribute value so that the &t;form&gt; posts as normal."
